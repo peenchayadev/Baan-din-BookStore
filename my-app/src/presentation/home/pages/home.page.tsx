593a768f4a3707a31d9b9abe838093c13c565da7
Peenchayakorn <148639119+peenchayadev@users.noreply.github.com>
2024-09-18 17:28:16 +0700

    create

---
+++ my-app/src/presentation/home/pages/home.page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const HomePage = () => {
+  //---------------------
+  //   CONST
+  //---------------------
+  const book = [
+    "Harry Potter and the Philosopher's Stone",
+    'Harry Potter and the Chamber of Secrets',
+    'Harry Potter and the Prisoner of Azkaban',
+    'Harry Potter and the Goblet of Fire',
+    'Harry Potter and the Order of the Phoenix',
+    'Harry Potter and the Half-Blood Prince',
+    'Harry Potter and the Deathly Hallows',
+  ]
+  //---------------------
+  //   RENDER
+  //---------------------
+  return <div className="text-red-500">sssss</div>
+}
